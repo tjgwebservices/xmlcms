884d014c53c6b00033c479cec1163ba746a8155f
tjgwebservices <info@tjgwebservices.com>
2020-09-17 21:19:32 -0500

    previous commit

--- src/main/java/com/tjgwebservices/tjgxmlcms/dbm/HibernateAdmin.java
+++ src/main/java/com/tjgwebservices/tjgxmlcms/dbm/HibernateAdmin.java
@@ -9,7 +9,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
